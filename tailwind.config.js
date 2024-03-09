033736c1f0ff8778196c4aab2da5667d421fdb2f
farzanafatema <farzan.bhuiyan@gmail.com>
2024-03-09 17:25:55 +0600

    added news to footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,158 @@ module.exports = {
     },
     plugins: [],
 }
+/*      <!-- product 2 -->
+                <div class="flex flex-1 gap-4 ">
+                    <div>
+                        <img src="Images/6.png" alt="">
+                    </div>
+                    <div class="flex-1 space-y-2">
+                        <div class="inline-flex gap-5 ">
+                            <h3 class=" text-lg "><i class="fa-solid fa-star mr-2"></i>5.00</h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-eye mr-2"></i>20
+                            </h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-heart mr-2"></i>200
+                            </h3>
+
+
+                        </div>
+                        <div class="space-y-2">
+                            <h3 class="text-xl font-extrabold">World Cup Flags Football</h3>
+                            <p>
+                                In World Cup Flags Football, players compete using flags representing different nations,
+                                adding a unique twist to the traditional game of football.</p>
+                            <div class="flex space-x-4">
+                                <h2 class="text-xl font-extrabold">Price - $22.00</h2>
+                                <i class="fa-solid fa-cart-shopping text-xl"></i>
+                                <h3 class="text-xl">Delivery Fee : Free</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- product 3 -->
+                <div class="flex flex-1 gap-4 ">
+                    <div>
+                        <img src="Images/7.png" alt="">
+                    </div>
+                    <div class="flex-1 space-y-2">
+                        <div class="inline-flex gap-5 ">
+                            <h3 class=" text-lg "><i class="fa-solid fa-star mr-2"></i>5.00</h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-eye mr-2"></i>20
+                            </h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-heart mr-2"></i>200
+                            </h3>
+
+
+                        </div>
+                        <div class="space-y-2">
+                            <h3 class="text-xl font-extrabold">World Cup Flags Football</h3>
+                            <p>
+                                In World Cup Flags Football, players compete using flags representing different nations,
+                                adding a unique twist to the traditional game of football.</p>
+                            <div class="flex space-x-4">
+                                <h2 class="text-xl font-extrabold">Price - $22.00</h2>
+                                <i class="fa-solid fa-cart-shopping text-xl"></i>
+                                <h3 class="text-xl">Delivery Fee : Free</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- product 4 -->
+                <div class="flex flex-1 gap-4 ">
+                    <div>
+                        <img src="Images/8.png" alt="">
+                    </div>
+                    <div class="flex-1 space-y-2">
+                        <div class="inline-flex gap-5 ">
+                            <h3 class=" text-lg "><i class="fa-solid fa-star mr-2"></i>5.00</h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-eye mr-2"></i>20
+                            </h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-heart mr-2"></i>200
+                            </h3>
+
+
+                        </div>
+                        <div class="space-y-2">
+                            <h3 class="text-xl font-extrabold">World Cup Flags Football</h3>
+                            <p>
+                                In World Cup Flags Football, players compete using flags representing different nations,
+                                adding a unique twist to the traditional game of football.</p>
+                            <div class="flex space-x-4">
+                                <h2 class="text-xl font-extrabold">Price - $22.00</h2>
+                                <i class="fa-solid fa-cart-shopping text-xl"></i>
+                                <h3 class="text-xl">Delivery Fee : Free</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- product 5 -->
+                <div class="flex flex-1 gap-4 ">
+                    <div>
+                        <img src="Images/9.png" alt="">
+                    </div>
+                    <div class="flex-1 space-y-2">
+                        <div class="inline-flex gap-5 ">
+                            <h3 class=" text-lg "><i class="fa-solid fa-star mr-2"></i>5.00</h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-eye mr-2"></i>20
+                            </h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-heart mr-2"></i>200
+                            </h3>
+
+
+                        </div>
+                        <div class="space-y-2">
+                            <h3 class="text-xl font-extrabold">World Cup Flags Football</h3>
+                            <p>
+                                In World Cup Flags Football, players compete using flags representing different nations,
+                                adding a unique twist to the traditional game of football.</p>
+                            <div class="flex space-x-4">
+                                <h2 class="text-xl font-extrabold">Price - $22.00</h2>
+                                <i class="fa-solid fa-cart-shopping text-xl"></i>
+                                <h3 class="text-xl">Delivery Fee : Free</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- product 6 -->
+                <div class="flex flex-1 gap-4 ">
+                    <div>
+                        <img src="Images/10.png" alt="">
+                    </div>
+                    <div class="flex-1 space-y-2">
+                        <div class="inline-flex gap-5 ">
+                            <h3 class=" text-lg "><i class="fa-solid fa-star mr-2"></i>5.00</h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-eye mr-2"></i>20
+                            </h3>
+                            <h3 class="text-lg ">
+                                <i class="fa-solid fa-heart mr-2"></i>200
+                            </h3>
+
+
+                        </div>
+                        <div class="space-y-2">
+                            <h3 class="text-xl font-extrabold">World Cup Flags Football</h3>
+                            <p>
+                                In World Cup Flags Football, players compete using flags representing different nations,
+                                adding a unique twist to the traditional game of football.</p>
+                            <div class="flex space-x-4">
+                                <h2 class="text-xl font-extrabold">Price - $22.00</h2>
+                                <i class="fa-solid fa-cart-shopping text-xl"></i>
+                                <h3 class="text-xl">Delivery Fee : Free</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>*/
